cf768783a9fc12202336cdd073f7c5146a85378d
Yonghee Lee <leo@shingu.ac.kr>
2025-09-03 12:09:16 +0900

    01show

---
+++ 01begin.py
@@ -0,0 +1,12 @@
+import cv2
+
+# 이미지 파일 읽기
+img_file = "./img/HM.jpg"  # 표시할 이미지 경로            ---①
+img = cv2.imread(img_file)  # 이미지를 읽어서 img 변수에 할당 ---②
+
+if img is not None:
+    cv2.imshow('IMG', img)  # 읽은 이미지를 화면에 표시      --- ③
+    cv2.waitKey()  # 키가 입력될 때 까지 대기      --- ④
+    cv2.destroyAllWindows()  # 창 모두 닫기            --- ⑤
+else:
+    print('No image file.')
